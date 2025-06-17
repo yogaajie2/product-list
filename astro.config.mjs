b26297318ff800b37a19ac48cda329f7c367b3dc
Yoga Prasetya <yogaajie2@gmail.com>
2025-06-17 20:28:05 +0700

    build: remove preact & svgr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 import tailwindcss from "@tailwindcss/vite";
-import svgr from "vite-plugin-svgr";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact({ compat: true })],
-
   vite: {
-    plugins: [svgr(), tailwindcss()],
+    plugins: [tailwindcss()],
   },
 });
