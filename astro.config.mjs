ffb727ba431cc45f6f714d3522307897afda1bf8
Yoga Prasetya <yogaajie2@gmail.com>
2025-06-18 11:10:29 +0700

    build: add vite-svg-loader

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
+import svgLoader from "vite-svg-loader";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), svgLoader()],
   },
 
   integrations: [vue()],
